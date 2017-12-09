980d08adbe0619da3d3aec1fe686306b92203793
MMTDIGITAL\einars.v <einars.v@mmtdigital.co.uk>
2017-12-09 17:22:48 +0000

    Initial commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+  public class CustomersController : Controller
+  {
+    // GET: Customers
+    public ActionResult Index()
+    {
+      var customers = GetCustomers();
+
+
+      return View(customers);
+    }
+
+    public ActionResult Details(int id)
+    {
+      var customer = GetCustomers().SingleOrDefault(person => person.Id == id);
+
+      if (customer == null)
+      {
+        return HttpNotFound();
+      }
+
+      return View(customer);
+    }
+
+    private IEnumerable<Customer> GetCustomers()
+    {
+      return new List<Customer>()
+      {
+        new Customer(){Id = 0, Name = "Einars"},
+        new Customer(){Id = 1, Name = "Edite"},
+        new Customer(){Id = 2, Name = "Daniels"},
+        new Customer(){Id = 3, Name = "Alise"},
+        new Customer(){Id = 4, Name = "Kristians"}
+      };
+    }
+  }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.MappingViews;
+using System.Web.Mvc;
+using System.Web.WebSockets;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+  public class MoviesController : Controller
+  {
+    // GET: Movies
+    public ActionResult Index()
+    {
+      var movie = new List<Movie>()
+      {
+        new Movie(){Name = "Thor"},
+        new Movie(){Name = "Iron Man"}
+      };
+      return View(movie);
+    }
+    public ActionResult Random()
+    {
+      var movie = new Movie() { Name = "Ragnarok" };
+      var customers = new List<Customer>
+      {
+        new Customer(){Name = "Einars"},
+        new Customer(){Name = "Edite"}
+      };
+      var viewModel = new RandomMovieViewModel
+      {
+        Movie = movie,
+        Customers = customers
+      };
+
+      return View(viewModel);
+      //return Content("Hello");
+      //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+
+    }
+
+    [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+    public ActionResult ByReleseDate(int year, int month)
+    {
+      return Content(year + "/" + month);
+    }
+  }
+}
